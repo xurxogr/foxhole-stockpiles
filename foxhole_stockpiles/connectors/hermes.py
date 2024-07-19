947657f8736a38540cf59d5d4a6f1783a6743ca5
Xurxogr <jgroca@gmail.com>
2024-07-19 19:40:02 +0200

    feat(hermes): Process error messages from Hermes before sending them back to the client.
    use the prefix FS for errors generated by the app so they can be distinguised from the ones from Hermes

--- foxhole_stockpiles/connectors/hermes.py
+++ foxhole_stockpiles/connectors/hermes.py
@@ -18,24 +18,29 @@ class HermesConnector():
         :param api_key: str = API_KEY header to use for authentication
         """
         if not stockpile:
-            return { "message": "Stockpile is Empty" }
+            return { "message": "FS: Stockpile is Empty" }
 
         if not api_key:
-            return { "message": "API key not set" }
+            return { "message": "FS: API key not set" }
         
         if not self.__url:
-            return { "message": "URL is not set" }
+            return { "message": "FS: URL is not set" }
 
         headers = { "X-API-TOKEN": api_key }
         return_data = {}
-        async with AsyncClient(verify=False, headers=headers) as client:
-            response = await client.post(url=self.__url, json=stockpile)
-            if response.status_code == 200:
-                try:
-                    return_data = response.json()
-                except:
+        try:
+            async with AsyncClient(verify=False, headers=headers) as client:
+                response = await client.post(url=self.__url, json=stockpile)
+                if response.status_code == 200:
+                    try:
+                        return_data = response.json()
+                        # If the response is an error, return the error message, else return the response in message or the json response
+                        return_data = return_data.get('error', return_data.get('message', return_data))
+                    except:
+                        return_data = { "message": response.text }
+                else:
                     return_data = { "message": response.text }
-            else:
-                return_data = { "message": response.text }
+        except Exception as e:
+            return_data = { "message": "FS: Error sending stockpile to the backend server: {}".format(str(e)) }
 
         return return_data
