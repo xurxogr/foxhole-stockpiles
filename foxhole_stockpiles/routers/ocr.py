849c24ace66ff091722c3debe025ca461bc8cb32
Xurxogr <jgroca@gmail.com>
2025-03-16 18:30:32 +0100

    refactor: refactor scan_image method in orc router to reduce its cognitive complexity

--- foxhole_stockpiles/routers/ocr.py
+++ foxhole_stockpiles/routers/ocr.py
@@ -10,87 +10,150 @@ from fastapi import APIRouter, Request, UploadFile
 from foxhole_stockpiles.connectors.backend import BackendConnector
 from foxhole_stockpiles.core.config import settings
 from foxhole_stockpiles.models.stockpile import Stockpile
-from foxhole_stockpiles.models.stockpile_item import StockpileItem
 from foxhole_stockpiles.services.ocr import OCR
 
 ocr_router = APIRouter()
 
 
 @ocr_router.post("/scan_image")
-async def __scan_image(image: UploadFile, request: Request):
-    """Scan an image and extract stockpile information."""
+async def scan_image(image: UploadFile, request: Request) -> dict:
+    """Scan an image and extract stockpile information.
+
+    Args:
+        image (UploadFile): Image to scan
+        request (Request): Request information
+
+    Returns:
+        dict: Stockpile information
+    """
     logger = logging.getLogger(__name__)
     api_key = request.headers.get("API_KEY")
     if not api_key:
-        message = "No api key"
-        logger.info(message)
-        return {"message": message}
+        return await log_and_return(message="No api key")
 
     if image is None:
-        message = "No input image"
-        logger.info(message)
-        return {"message": message}
+        return await log_and_return(message="No input image")
 
-    bytes_as_np_array = numpy.frombuffer(await image.read(), dtype=numpy.uint8)
-    image = cv2.imdecode(buf=bytes_as_np_array, flags=cv2.IMREAD_COLOR)
+    image = await read_image(image=image)
 
     ocr = OCR()
     start = time.time()
-    stockpile: Stockpile = await ocr.extract_stockpile_from_image(
-        image=image, file_name=api_key[:10]
-    )
+    stockpile = await ocr.extract_stockpile_from_image(image=image, file_name=api_key[:10])
     end = time.time()
 
     if not stockpile:
-        message = "No stockpile found in the image"
-        logger.info(message)
-        return {"message": message}
+        return await log_and_return(message="No stockpile found in the image")
 
     width = image.shape[1]
     height = image.shape[0]
 
     if not stockpile.items:
-        logger.info(
-            f"{stockpile.type}:{stockpile.name} ({width}x{height}). "
-            f"Scanned image in {end - start:.2f}. No items found in the image."
+        return await log_and_return(
+            message=f"{stockpile.name}: No items found in the image",
+            width=width,
+            height=height,
+            start=start,
+            end=end,
         )
-        return {"message": f"{stockpile.name}: No items found in the image"}
 
     logger.info(
-        f"{stockpile.type}:{stockpile.name} ({width}x{height}). Scanned image in {end - start:.2f}."
+        "%s:%s (%dx%d). Scanned image in %.2f.",
+        stockpile.type,
+        stockpile.name,
+        width,
+        height,
+        end - start,
     )
 
+    stockpile_dict = await create_stockpile_dict(stockpile=stockpile)
+    return send_stockpile(stockpile=stockpile_dict, api_key=api_key)
+
+
+async def log_and_return(
+    message: str, width: int = None, height: int = None, start: time = None, end: time = None
+):
+    """Log a message and return it.
+
+    Args:
+        message (str): Message to log and return
+        width (int, optional): Width of the image. Defaults to None.
+        height (int, optional): Height of the image. Defaults to None.
+        start (time, optional): Start time. Defaults to None.
+        end (time, optional): End time. Defaults to None.
+
+    Returns:
+        dict: Message
+    """
+    logger = logging.getLogger(__name__)
+    if width and height and start and end:
+        logger.info("%s (%dx%d). Scanned image in %.2f.", message, width, height, end - start)
+    else:
+        logger.info(message)
+    return {"message": message}
+
+
+async def read_image(image: UploadFile) -> numpy.ndarray:
+    """Read an image from an UploadFile.
+
+    Args:
+        image (UploadFile): Image to read
+
+    Returns:
+        numpy.ndarray: Image as a numpy array
+    """
+    bytes_as_np_array = numpy.frombuffer(buffer=await image.read(), dtype=numpy.uint8)
+    return cv2.imdecode(buf=bytes_as_np_array, flags=cv2.IMREAD_COLOR)
+
+
+async def create_stockpile_dict(stockpile: Stockpile) -> dict:
+    """Create a stockpile dictionary from a Stockpile.
+
+    Args:
+        stockpile (Stockpile): Stockpile to convert
+
+    Returns:
+        dict: Stockpile dictionary
+    """
     items = []
     items_no_quantity = []
-    item: StockpileItem
     for item in stockpile.items:
         if item.quantity == -1:
             items_no_quantity.append(item.code)
 
         items.append({"code": item.code, "quantity": item.quantity, "crated": item.crated})
 
-    stockpile_dict = {
+    if items_no_quantity:
+        items_text = ", ".join(items_no_quantity)
+        logger = logging.getLogger(__name__)
+        logger.error("%s: Items without quantity: %s", stockpile.name, items_text)
+
+    return {
         "stockpile_name": stockpile.name,
         "stockpile_type": stockpile.type,
         "resolution": stockpile.resolution,
         "items": items,
     }
 
-    if items_no_quantity:
-        items_text = ", ".join(items_no_quantity)
-        logger.error(f"{stockpile.name}: Items without quantity: {items_text}")
 
+async def send_stockpile(stockpile: dict, api_key: str) -> dict:
+    """Send a stockpile to the backend server.
+
+    Args:
+        stockpile (dict): Stockpile to send
+        api_key (str): API key to use for authentication
+    """
     url = settings.backend.url
-    if url and api_key.lower() != "debug":
-        connector = BackendConnector(url=url)
-        try:
-            return await connector.send_stockpile(payload=stockpile_dict, api_key=api_key)
-        except Exception as e:
-            message = (
-                f"Error sending stockpile {stockpile.name} ({stockpile.type}) "
-                f"to the backend server: {e.__class__.__name__}"
-            )
-            logger.error(message)
-            return {"message": message}
+    if not url or api_key.lower() == "debug":
+        return stockpile
 
-    return stockpile_dict
+    connector = BackendConnector(url=url)
+    try:
+        return await connector.send_stockpile(payload=stockpile, api_key=api_key)
+    except Exception as e:
+        message = (
+            f"Error sending stockpile {stockpile.get('stockpile_name')} "
+            f"({stockpile.get('stockpile_type')}) to the backend server: {e.__class__.__name__}"
+        )
+        logger = logging.getLogger(__name__)
+        logger.error(message)
+        return {"message": message}
